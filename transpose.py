1ae5883eee272289479da970993784457f980f74
harsha0321 <hvchilukuri@gmail.com>
2026-04-17 11:07:20 +0530

    made some changes

--- transpose.py
+++ transpose.py
@@ -7,5 +7,18 @@ def transpose(matrix):
             result[j][i]=matrix[i][j]
     return result
 
+
+
+
+def add(matrix):
+    row=len(matrix)
+    col=len(matrix[0])
+
+    result=[[0 for i in range (row)] for j in range(col)]
+    for i in range(row):
+        for j in range(col):
+            result[j][i]=matrix[i][j]+1
+    return result
+
 matrix=[[1,2,3],[4,5,6],[7,8,9]]
-print(transpose(matrix))
+print(add(matrix))
